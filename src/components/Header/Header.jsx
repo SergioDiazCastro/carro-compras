a58d6d86073c02096d275259e502f5eca02bd43e
Sergio Diaz <sydiazcastro@gmail.com>
2023-09-14 22:28:57 -0500

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import carrito from '../Header/images/carro-de-la-compra.png'
 import { Logo } from "../Logo/Logo"
 import { NavLink } from 'react-router-dom'
+import Modal from 'react-modal'
 
 
-export const Header = ({count}) =>{
+
+
+
+
+export const Header = ({count, selectedProduct}) =>{
+    const [modalIsOpen, SetModalIsOpen] = useState(false);
     return(
-        <div className='bg-blue-950 flex justify-around'>
+        <div className='bg-blue-950 flex justify-center items-center '>
             <Logo></Logo>
-            <nav>
+    <nav>
         <ul className='flex items-center text-2xl  font-semibold text-white'>
           <li className='w-[100%]'><NavLink to='/'>Inicio</NavLink></li> 
-          <li className='flex'><img className='w-[25%]' src={carrito} />
-          <p className=" rounded-full w-min  text-[15px]">{count}</p></li>
-        </ul>
+          <li className='flex h-full'><img onClick={() => SetModalIsOpen(true)} className='w-[25%]' src={carrito} />
+          <p className="items-center text-[15px] w-4 text-white ">{count}</p></li>
+        </ul> 
     </nav>
+          
+          <Modal isOpen={modalIsOpen} onRequestClose={() => SetModalIsOpen(false)}
+            className='mx-auto my-20 p-4 bg-blue-950 w-[30%]'>
+           <li className='text-white '>{selectedProduct +"\n"}</li>
+           
+        <button onClick={() => SetModalIsOpen(false)} 
+        className='bg-white w-1/2 flex self-center justify-center mt-10'>Cerrar</button>
+        </Modal>
+        
         </div>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,21 +6,32 @@ import { Header } from "../Header/Header";
 export const Main = () => {
 
   const [products, setProducts] = useState([]);
-  const [selectedCategory,] = useState("");
+  const [selectedTitle] = useState("");
   const [count, setCount] = useState(0);
+  const [selectedProduct, setSelectedProduct] = useState([]);
 
-  const filterProducts = (category) => {
-    if (category === "") {
-      return products; 
-    } else {
-      return products.filter((product) => product.category === category);
+  const filterProducts = (title, productName) => {
+  if (title === "") {
+    return products;
+  } else {
+    const filteredProducts = products.filter(
+      (product) => product.title === title
+    );
+    if (productName) {
+      const selected = filteredProducts.find(
+        (product) => product.title === productName
+      );
+      if (selected) {
+        setSelectedProduct(selected.title);
+      }
     }
-  };
+    return filterProducts;
+  }
+};
   useEffect(() => {
     const getProducts = async () => {
       const url = "https://fakestoreapi.com/products";
       const results = await axios.get(url);
-
       setProducts(results.data);
       console.log(results);
     };
@@ -30,11 +41,10 @@ export const Main = () => {
 
     return (
       <>
-      
-      <Header count={count}></Header>
+      <Header count={count} selectedProduct={selectedProduct}></Header>
       <div className="flex flex-wrap gap-5 justify-center items-center">
         {products.length === 0 }
-        {filterProducts(selectedCategory).map((product, i) => {
+        {filterProducts(selectedTitle).map((product, i) => {
           return (
             <>
               <div className="flex flex-col justify-center bg-blue-950 items-center w-[300px] h-[400px] mt-5 border-4 border-black"
@@ -43,7 +53,7 @@ export const Main = () => {
                 <div className="w-full p-2 flex justify-end items-center">
                   
                 </div>
-                <img className=" w-[90%] h-[40%]" src={product.image}
+                <img className="   w-[90%] h-[40%]" src={product.image}
                 />
                 <h2 className="my-4 text-center w-full text-white font-normal">{product.title}</h2>
                 <div className="flex flex-col text-center gap-2">
@@ -51,7 +61,8 @@ export const Main = () => {
                   <h4 className="text-white font-normal">
                     Precio: {product.price}
                   </h4>
-                  <button className="bg-white border-2 rounded-lg"onClick={() => setCount(count + 1)}>añadir</button>
+                  <button className="bg-white border-2 rounded-lg" onClick={() => {setCount(count + 1);
+                  setSelectedProduct(selectedProduct +  product.title)}}> Añadir</button>
                 </div>
               </div>
             </>
